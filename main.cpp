31f0bf18df4552406cb5bc30fcfcbf6b77edb555
Marcel Chołodecki <cholodecki@gmail.com>
2024-12-26 17:27:51 +0100

    Experimenting with the QMenuBar class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "contentview.h"
 #include <QApplication>
+#include <QMenuBar>
 
 // Created by Marcel Cholodecki on 2024.12.24
 
@@ -9,6 +10,12 @@ int main(int argc, char *argv[])
     QApplication mainApp(argc, argv);
     MainWindow mainWindow;
     mainWindow.setWindowTitle("AX Plotter");
+
+    QMenuBar* menubar = new QMenuBar(&mainWindow);
+
+    menubar->addAction("File");
+
+    mainWindow.setMenuBar(menubar);
 
     mainWindow.setCentralWidget(new ContentView());
 
