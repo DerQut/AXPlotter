e3034872ef1b9980377916be1760570f82ea6295
Marcel Chołodecki <cholodecki@gmail.com>
2025-01-03 20:54:48 +0100

    Added comments

--- themefile.cpp
+++ themefile.cpp
@@ -16,16 +16,19 @@ Theme getTheme(const char* fileName) {
     ifstream file;
     file.open(fileName);
 
+    // Error handling: file missing
     if (!file.is_open()) {
         qDebug() << "Creating file";
         saveTheme(fileName, DEFAULT);
         return DEFAULT;
     }
 
+    // Reading data from file
     getline(file, line);
     file.close();
     qDebug() << "File read succesfully: " << QString::fromStdString(line);
 
+    // Parsing std::string to Theme
     if (line == "DARK")
         return DARK;
 
@@ -36,6 +39,7 @@ void saveTheme(const char* fileName, Theme theme) {
 
     string line;
 
+    // Parsing Theme to std::string
     switch (theme) {
     case DEFAULT:
         line = "DEFAULT";
@@ -49,11 +53,13 @@ void saveTheme(const char* fileName, Theme theme) {
     ofstream file;
     file.open(fileName);
 
+    // Error handling
     if (!file.is_open()) {
         qDebug() << "Error writing to file";
         return;
     }
 
+    // Reading Theme to file
     file << line;
     file.close();
 
