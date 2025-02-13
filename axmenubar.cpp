e5ef266162e902c6b8213f9072e2dce85cc6be0f
Marcel Chołodecki <cholodecki@gmail.com>
2025-02-13 04:13:01 +0100

    Added comments

--- axmenubar.cpp
+++ axmenubar.cpp
@@ -17,6 +17,7 @@ AXMenuBar::AXMenuBar(ContentView* parent) :
 {
     contentView = parent;
 
+    // Creating QMenu instances
     QMenu* fileMenu = new QMenu("&File", this);
     this->addMenu(fileMenu);
 
@@ -36,21 +37,29 @@ AXMBViewMenu::AXMBViewMenu(const QString &title, AXMenuBar* parent) :
 {
     axMenuBar = parent;
 
+    // Creating an exclusive QButtonGroup fot the Theme picker
     QButtonGroup* themeGroup = new QButtonGroup(this);
     themeGroup->setExclusive(1);
 
+    // Creating QCheckBox instances for the Theme picker
     defaultThemeCheck = new QCheckBox("Default", this);
     darkThemeCheck = new QCheckBox("Dark", this);
     classicThemeCheck = new QCheckBox("Classic", this);
 
+    /*
+     * Adding the created QCheckBox instances to the QButtonGroup instance
+     * using the Theme enum as the button ID
+    */
     themeGroup->addButton(defaultThemeCheck, DEFAULT);
     themeGroup->addButton(darkThemeCheck, DARK);
     themeGroup->addButton(classicThemeCheck, CLASSIC);
 
+    // Checking the appropriate check box at startup
     defaultThemeCheck->setChecked(1);
     darkThemeCheck->setChecked(getTheme("theme.cfg") == DARK);
     classicThemeCheck->setChecked(getTheme("theme.cfg") == CLASSIC);
 
+    // Creating a QVBoxLayout instance to host the buttons inside the theme sub-menu
     QVBoxLayout* themeVStack = new QVBoxLayout(this);
     themeVStack->setSpacing(0);
     themeVStack->addWidget(defaultThemeCheck);
